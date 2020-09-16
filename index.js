d30c0698d92d21946a3c918374a18fe25fb3c736
Stas <stanislaw.spirin@ya.ru>
2020-09-16 18:00:01 +0300

    bafore-migrate

--- index.js
+++ index.js
@@ -10,38 +10,40 @@ app.use(bodyParser.json())
 app.listen(33310);
 
 // Get all
-app.get('/api/posts', function(req, res){
-    Post.find({}, function(err, result){
+app.get('/api/posts', function (req, res) {
+    Post.find({}, function (err, result) {
         if (err) throw err
         res.json(result)
     });
 });
 
 //Get one
-app.get('/api/posts/:alias', function(req, res){
-    Post.findOne({alias: req.params.alias}, function(err, result){
+app.get('/api/posts/:alias', function (req, res) {
+    Post.findOne({ alias: req.params.alias }, function (err, result) {
         if (err) throw err
         res.json(result)
     });
 });
 
 // Add Post
-app.put('/api/posts/', function(req, res){
+app.put('/api/posts/', function (req, res) {
     const body = req.body;
     var post = new Post(body)
     try {
-    post.save(function (err) {
-        console.log(err)
-    })
-}catch (err){
-    throw err
-}
+        post.save(function (err) {
+            if (err) {
+                console.log(err)
+            } else console.log('OK')
+        })
+    } catch (err) {
+        throw err
+    }
     res.end('OK')
 })
 
 //Delete post
-app.delete('/api/posts/:alias', cors({origin: 'http://localhost:3000'}), function(req, res){
-    Post.deleteOne({alias: req.params.alias}, function(err){
+app.delete('/api/posts/:alias', cors({ origin: 'http://localhost:3000' }), function (req, res) {
+    Post.deleteOne({ alias: req.params.alias }, function (err) {
         if (err) throw err
     })
     res.end('OK')
